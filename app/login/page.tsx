f22ae1552fb8d905f15d17854c3fc1836dc2d009
akemi27 <sosapurisaca.ma@gmail.com>
2026-05-29 13:13:17 -0500

    revisión

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
         setMensaje("Conectando");
 
         try {
-            const res = await fetch("/login", {
+            const res = await fetch("/api/login", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
                 body: JSON.stringify({username, password})
